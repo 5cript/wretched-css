30dc0695ab1d44dc8ed393bb57c8a271e3dc21e5
5cript <tim06tr@gmail.com>
2016-02-23 16:49:49 +0100

    Added development project.

---
+++ debug/main.cpp
@@ -0,0 +1,33 @@
+#include "main.hpp"
+
+#include "../rule_set/rule_set.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    using namespace WretchedCss;
+
+    auto data = readStringFromFile ("testfile.txt");
+
+    RuleSet set;
+    set.fromCss(data);
+
+    return 0;
+}
+
+std::string readStringFromFile(std::string const& fileName)
+{
+    std::ifstream reader (fileName, std::ios_base::binary);
+
+    std::string data;
+    do {
+        char buffer[4096];
+        reader.read(buffer, 4096);
+        data.append(buffer, reader.gcount());
+    } while (reader.gcount() == 4096);
+
+    return data;
+}
